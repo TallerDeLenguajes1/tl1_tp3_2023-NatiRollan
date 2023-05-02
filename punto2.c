d9d46feae78f09d016f63a3ee5ebcfe604306d4a
“NatiRollan” <natyangela2010@gmail.com>
2023-05-02 02:17:53 -0300

    Punto 2 completo

--- punto2.c
+++ punto2.c
@@ -1,69 +1,97 @@
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 5
 #define M 12
 
-int main(){
-    int matriz[N][M], suma, minimo, maximo, anio, mes, mayor=0, menor=1000001, mes_menor, anio_menor;
-    float promedio;
+void cargarMatriz (int matriz[N][M]);
+void mostrarMatriz (int matriz[N][M]);
+void promedio (int matriz[N][M]);
+void valorMaxMin (int matriz[N][M]);
 
-    srand(time(NULL));
+int main ()
+{
+    int produccion[N][M];
 
     //Cargue dicha matriz con valores aleatorios entre 10000 y 50000.
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            matriz[i][j] = rand() % 50000 + 10000;
-        }
-    }
+    srand(time(NULL));
+    cargarMatriz(produccion);
 
     //Muestre por pantalla los valores cargados
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            printf("%6d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
+    mostrarMatriz(produccion);
 
     //Saque el promedio de ganancia por año y muestrelos por pantalla
-    for (int i = 0; i < N; i++)
-    {
-        suma = 0;
-        for (int j = 0; j < M; j++)
-        {
-           suma = suma + matriz[i][j];
-        }
-        promedio = suma/M;
-        printf("\nPromedio de ganancia del anio %d: %.2f", i+1, promedio);
-    }
+    promedio(produccion);
 
     //Obtenga el valor máximo y el valor mínimo obtenido informando el “año” y el “mes” de cuándo ocurrió.
+    valorMaxMin(produccion);
+
+    return 0;
+}
+
+void cargarMatriz (int matriz[N][M])
+{
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
         {
-            if (matriz[i][j] > mayor)
+            matriz[i][j] = 10000 + rand()%50000;
+        }
+    }
+}
+
+void mostrarMatriz (int matriz[N][M])
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < M; j++)
+        {
+            printf("%8d", matriz[i][j]);
+        }
+    }
+}
+
+void promedio (int matriz[N][M])
+{
+    int suma=0, promedios;
+    for (int i = 0; i < N; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < M; j++)
+        {
+            suma = suma + matriz[i][j];
+        }
+        promedios = suma/M;
+        printf("\nGanacia del anio %d: %d", i+1, promedios);
+    }
+}
+
+void valorMaxMin (int matriz[N][M])
+{
+    int valorMax = 10000, valorMin = 50000, anio_min, anio_max, mes_min, mes_max;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (matriz[i][j] > valorMax)
             {
-                mayor = matriz[i][j];
-                mes = j+1;
-                anio = i+1;
+                valorMax = matriz[i][j];
+                anio_max = i + 1;
+                mes_max = j + 1;
             }
 
-            if (matriz[i][j] < menor)
+            if (matriz[i][j] < valorMin)
             {
-                menor = matriz[i][j];
-                mes_menor = j+1;
-                anio_menor = i+1;
+                valorMin = matriz[i][j];
+                anio_min = i + 1;
+                mes_min = j + 1;
             }
         }
     }
-    printf("\n\nEl valor maximo es %d, ocurrio en el mes %d, anio %d", mayor, mes, anio);
-    printf("\nEl valor minimo es %d, ocurrio en el mes %d, anio %d", menor, mes_menor, anio_menor);
 
-    return 0;
+    printf("\n\nEl valor maximo es: %d", valorMax);
+    printf("\nOcurrio en el anio %d, mes %d", anio_max, mes_max);
+    printf("\n\nEl valor minimo es: %d", valorMin);
+    printf("\nOcurrio en el anio %d, mes %d", anio_min, mes_min);
 }
